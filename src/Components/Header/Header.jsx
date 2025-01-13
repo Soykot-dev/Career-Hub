77a0f8aebb9bcabd2b9f9d9ec3bcb5cd9c0073e8
Sabbir Ahmed Soykot <soykotislam131415@gmail.com>
2025-01-13 19:09:44 +0600

    Improve header navigation styling and layout for better user experience

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
     const links = <>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/jobs">Jobs</NavLink></li>
-        <li><NavLink to="/appliedJobs">Applied Jobs</NavLink></li>
-        <li><NavLink to="/statistics">Statistics</NavLink></li>
-        <li><NavLink to="/blogs">Blogs</NavLink></li>
+        <li><NavLink to="/" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "text-[#808EFE] underline hover:text-[#6d6fff] font-semibold duration-100 hover:mr-1" : "text-gray-500 hover:text-black duration-100 hover:underline hover:mr-1"}>Home</NavLink></li>
+        <li><NavLink to="/jobs" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "text-[#808EFE] underline hover:text-[#6d6fff] font-semibold duration-100 hover:mx-1" : "text-gray-500 hover:text-black duration-100 hover:underline hover:mx-1"}>Jobs</NavLink></li>
+        <li><NavLink to="/appliedJobs" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "text-[#808EFE] underline hover:text-[#6d6fff] font-semibold duration-100 hover:mx-1" : "text-gray-500 hover:text-black duration-100 hover:underline hover:mx-1"}>Applied Jobs</NavLink></li>
+        <li><NavLink to="/statistics" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "text-[#808EFE] underline hover:text-[#6d6fff] font-semibold duration-100 hover:mx-1" : "text-gray-500 hover:text-black duration-100 hover:underline hover:mx-1"}>Statistics</NavLink></li>
+        <li><NavLink to="/blogs" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "text-[#808EFE] underline hover:text-[#6d6fff] font-semibold duration-100 hover:ml-1" : "text-gray-500 hover:text-black duration-100 hover:underline hover:ml-1"}>Blogs</NavLink></li>
     </>
     return (
         <div>
@@ -29,14 +29,14 @@ const Header = () => {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            className="menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-32 p-2 shadow">
                             {links}
                         </ul>
                     </div>
                     <a className="btn btn-ghost font-bold text-xl md:text-2xl">CareerHub</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
+                    <ul className="menu-horizontal gap-5 px-1">
                         {links}
                     </ul>
                 </div>
