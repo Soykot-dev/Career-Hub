d563c10f0849dcd6add4812d4d8a191ff7ff7db1
Sabbir Ahmed Soykot <soykotislam131415@gmail.com>
2025-01-13 13:13:36 +0600

    Update AppliedJobs component to display count of applied jobs from local storage

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,7 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+import { getStoredJobs } from "../../Utility/localStorage";
+import { useEffect, useState } from "react";
+
 const AppliedJobs = () => {
+    const [appliedJobs, setAppliedJobs] = useState([]);
+    const jobs = useLoaderData();
+    useEffect(() => {
+        const storeJob = getStoredJobs();
+        const jobApplied = jobs.filter(job => storeJob.includes(job.id))
+        setAppliedJobs(jobApplied);
+    }, [jobs])
     return (
         <div>
-            <h2>Job I applied</h2>
+            <h2>Job I applied {appliedJobs.length}</h2>
         </div>
     );
 };
