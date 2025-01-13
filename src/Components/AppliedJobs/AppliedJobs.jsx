aac993065172297be69a05116082dc96c71ea79a
Sabbir Ahmed Soykot <soykotislam131415@gmail.com>
2025-01-13 13:20:45 +0600

    Add background images and header to AppliedJobs component

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,6 +1,8 @@
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobs } from "../../Utility/localStorage";
 import { useEffect, useState } from "react";
+import bg1 from '../../assets/images/bg1.png'
+import bg2 from '../../assets/images/bg2.png'
 
 const AppliedJobs = () => {
     const [appliedJobs, setAppliedJobs] = useState([]);
@@ -12,6 +14,11 @@ const AppliedJobs = () => {
     }, [jobs])
     return (
         <div>
+            <div className="relative flex justify-center items-center bg-[#F9F9FF] rounded-md h-56">
+                <img className="absolute left-0 bottom-0" src={bg1} alt="" />
+                <h3 className="text-2xl font-semibold">Job Details</h3>
+                <img className="absolute right-0 top-0" src={bg2} alt="" />
+            </div>
             <h2>Job I applied {appliedJobs.length}</h2>
         </div>
     );
